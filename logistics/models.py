a07f434da90e5fe7fff02de864dcd345575dfe78
shkiiper <shygaev.xxx@gmail.com>
2023-05-11 13:17:26 +0600

    first commit

--- logistics/models.py
+++ logistics/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from orders.models import Order
+from datetime import date
 
 
 class Delivery(models.Model):
@@ -7,6 +8,15 @@ class Delivery(models.Model):
     address = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=20)
     order = models.ForeignKey(Order, on_delete=models.CASCADE, default=0)
+    date_from = models.DateField(null=True)
+    date_to = models.DateField(null=True)
+    STATUS_CHOICES = (
+        ('past', 'Past'),
+        ('now', 'Now'),
+        ('planned', 'Planned'),
+    )
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES,
+                              default='now')
 
     def __str__(self):
         return self.name
